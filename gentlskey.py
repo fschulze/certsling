f81d1fe6e1af33a346ff3b474b8f32c6cc9ef589
Florian Schulze <florian.schulze@gmx.net>
2015-12-11 13:53:24 +0100

    Use pathlib.

--- gentlskey.py
+++ gentlskey.py
@@ -1,15 +1,15 @@
+from pathlib import Path
 import click
 import datetime
-import os
 import subprocess
 import sys
 
 
-BASE = os.path.dirname(os.path.abspath(sys.argv[0]))
+BASE = Path(sys.argv[0]).absolute().parent.parent
 CURL = 'curl'
 OPENSSL = 'openssl'
-OPENSSL_CONF = '/usr/local/etc/openssl/openssl.cnf'
-SIGN_CSR = os.path.join(BASE, '..', 'letsencrypt-nosudo', 'sign_csr.py')
+OPENSSL_CONF = Path('/usr/local/etc/openssl/openssl.cnf')
+SIGN_CSR = BASE.joinpath('letsencrypt-nosudo', 'sign_csr.py')
 
 
 def fatal(msg, code=3):
@@ -18,33 +18,33 @@ def fatal(msg, code=3):
 
 
 def genkey(key_base, date, main):
-    fn = os.path.join(key_base, "%s.key" % main)
-    if os.path.exists(fn):
-        print("Using existing key '%s'." % os.path.relpath(fn))
+    fn = key_base.joinpath("%s.key" % main)
+    if fn.exists():
+        print("Using existing key '%s'." % fn.relative_to(Path.cwd()))
         return fn
-    fn_date = os.path.join(key_base, "%s-%s.key" % (main, date))
-    if not os.path.exists(fn_date):
-        print("Generating key '%s'." % os.path.relpath(fn_date))
+    fn_date = key_base.joinpath("%s-%s.key" % (main, date))
+    if not fn_date.exists():
+        print("Generating key '%s'." % fn_date.relative_to(Path.cwd()))
         subprocess.check_call([
-            OPENSSL, 'genrsa', '-out', fn_date, '4096'])
-    if os.path.exists(fn_date):
-        print("Linking key '%s'." % os.path.relpath(fn_date))
-        os.symlink(os.path.basename(fn_date), fn)
+            OPENSSL, 'genrsa', '-out', str(fn_date), '4096'])
+    if fn_date.exists():
+        print("Linking key '%s'." % fn_date.relative_to(Path.cwd()))
+        fn.symlink_to(fn_date.name)
     return fn
 
 
 def gencsr(key_base, date, key, main, domains):
-    fn = os.path.join(key_base, "%s.csr" % main)
-    if os.path.exists(fn):
-        print("Using existing csr '%s'." % os.path.relpath(fn))
+    fn = key_base.joinpath("%s.csr" % main)
+    if fn.exists():
+        print("Using existing csr '%s'." % fn.relative_to(Path.cwd()))
         return fn
-    fn_date = os.path.join(key_base, "%s-%s.csr" % (main, date))
-    if not os.path.exists(fn_date):
-        print("Generating csr '%s'." % os.path.relpath(fn_date))
+    fn_date = key_base.joinpath("%s-%s.csr" % (main, date))
+    if not fn_date.exists():
+        print("Generating csr '%s'." % fn_date.relative_to(Path.cwd()))
         if domains:
-            config_fn = os.path.join(key_base, 'openssl.cnf')
-            with open(config_fn, 'wb') as config:
-                with open(OPENSSL_CONF, 'rb') as f:
+            config_fn = key_base.joinpath('openssl.cnf')
+            with config_fn.open('wb') as config:
+                with OPENSSL_CONF.open('rb') as f:
                     data = f.read()
                     config.write(data)
                     if not data.endswith(b'\n'):
@@ -54,67 +54,69 @@ def gencsr(key_base, date, key, main, domains):
                 config.write(bytes('\n'.join(lines).encode('ascii')))
             subprocess.check_call([
                 OPENSSL, 'req', '-sha256', '-new',
-                '-key', key, '-out', fn_date, '-subj', '/', '-reqexts', 'SAN',
-                '-config', config_fn])
+                '-key', str(key), '-out', str(fn_date), '-subj', '/',
+                '-reqexts', 'SAN', '-config', str(config_fn)])
         else:
             subprocess.check_call([
                 OPENSSL, 'req', '-sha256', '-new',
-                '-key', key, '-out', fn_date, '-subj', '/CN=%s' % main])
-    if os.path.exists(fn_date):
-        print("Linking csr '%s'." % os.path.relpath(fn_date))
-        os.symlink(os.path.basename(fn_date), fn)
+                '-key', str(key), '-out', str(fn_date),
+                '-subj', '/CN=%s' % main])
+    if fn_date.exists():
+        print("Linking csr '%s'." % fn_date.relative_to(Path.cwd()))
+        fn.symlink_to(fn_date.name)
     return fn
 
 
 def verify(csr):
     subprocess.check_call([
-        OPENSSL, 'req', '-text', '-noout', '-verify', '-in', csr])
+        OPENSSL, 'req', '-text', '-noout', '-verify', '-in', str(csr)])
 
 
 def gencrt(key_base, date, csr, user_pub, main):
-    fn = os.path.join(key_base, "%s.crt" % main)
-    if os.path.exists(fn):
-        print("Using existing crt '%s'." % os.path.relpath(fn))
+    fn = key_base.joinpath("%s.crt" % main)
+    if fn.exists():
+        print("Using existing crt '%s'." % fn.relative_to(Path.cwd()))
         return fn
-    fn_date = os.path.join(key_base, "%s-%s.crt" % (main, date))
-    if not os.path.exists(fn_date):
-        print("Generating csr '%s'." % os.path.relpath(fn_date))
-        with open(fn_date, 'wb') as f:
+    fn_date = key_base.joinpath("%s-%s.crt" % (main, date))
+    if not fn_date.exists():
+        print("Generating csr '%s'." % fn_date.relative_to(Path.cwd()))
+        with fn_date.open('wb') as f:
             subprocess.check_call([
-                'python2.7', SIGN_CSR, '--public-key', user_pub, csr],
+                'python2.7', str(SIGN_CSR),
+                '--public-key', str(user_pub), str(csr)],
                 stdout=f)
-    if os.path.exists(fn_date):
-        print("Linking crt '%s'." % os.path.relpath(fn_date))
-        os.symlink(os.path.basename(fn_date), fn)
+    if fn_date.exists():
+        print("Linking crt '%s'." % fn_date.relative_to(Path.cwd()))
+        fn.symlink_to(fn_date.name)
     return fn
 
 
 def getpem(base, date):
     main = 'lets-encrypt-x1-cross-signed'
-    fn = os.path.join(base, "%s.pem" % main)
-    if os.path.exists(fn):
-        print("Using existing pem '%s'." % os.path.relpath(fn))
+    fn = base.joinpath("%s.pem" % main)
+    if fn.exists():
+        print("Using existing pem '%s'." % fn.relative_to(Path.cwd()))
         return fn
-    fn_date = os.path.join(base, "%s-%s.pem" % (main, date))
-    if not os.path.exists(fn_date):
-        print("Generating pem '%s'." % os.path.relpath(fn_date))
+    fn_date = base.joinpath("%s-%s.pem" % (main, date))
+    if not fn_date.exists():
+        print("Generating pem '%s'." % fn_date.relative_to(Path.cwd()))
         subprocess.check_call([
-            CURL, '-o', fn_date, 'https://letsencrypt.org/certs/%s.pem' % main])
-    if os.path.exists(fn_date):
-        print("Linking pem '%s'." % os.path.relpath(fn_date))
-        os.symlink(os.path.basename(fn_date), fn)
+            CURL, '-o', str(fn_date), 'https://letsencrypt.org/certs/%s.pem' % main])
+    if fn_date.exists():
+        print("Linking pem '%s'." % fn_date.relative_to(Path.cwd()))
+        fn.symlink_to(fn_date.name)
     return fn
 
 
 def chain(key_base, crt, pem, main):
-    fn = os.path.join(key_base, "%s-chained.crt" % main)
-    if os.path.exists(fn):
-        print("Using existing chained crt '%s'." % os.path.relpath(fn))
+    fn = key_base.joinpath("%s-chained.crt" % main)
+    if fn.exists():
+        print("Using existing chained crt '%s'." % fn.relative_to(Path.cwd()))
         return fn
-    print("Writing chained crt '%s'." % os.path.relpath(fn))
-    with open(fn, 'wb') as out:
+    print("Writing chained crt '%s'." % fn.relative_to(Path.cwd()))
+    with fn.open('wb') as out:
         for name in (crt, pem):
-            with open(name, 'rb') as f:
+            with name.open('rb') as f:
                 data = f.read()
                 out.write(data)
                 if not data.endswith(b'\n'):
@@ -123,19 +125,19 @@ def chain(key_base, crt, pem, main):
 
 
 def generate(base, domains):
-    user_pub = os.path.join(base, 'user.pub')
-    if not os.path.exists(user_pub):
+    user_pub = base.joinpath('user.pub')
+    if not user_pub.exists():
         fatal("No 'user.pub' in current directory.", code=10)
-    if not os.path.exists(SIGN_CSR):
+    if not SIGN_CSR.exists():
         fatal("No 'letsencrypt-nosudo' in '%s' directory." % BASE, code=10)
     domains = sorted(domains, key=len)
     main, domains = domains[0], domains[1:]
     for domain in domains:
         if not domain.endswith("." + main):
             fatal("Domain '%s' isn't a subdomain of '%s'.")
-    key_base = os.path.join(base, main)
-    if not os.path.exists(key_base):
-        os.mkdir(key_base)
+    key_base = base.joinpath(main)
+    if not key_base.exists():
+        key_base.mkdir()
     date = datetime.date.today().strftime("%Y%m%d")
     key = genkey(key_base, date, main)
     csr = gencsr(key_base, date, key, main, domains)
@@ -148,7 +150,7 @@ def generate(base, domains):
 @click.command()
 @click.argument("domain", nargs=-1)
 def main(domain):
-    base = os.getcwd()
+    base = Path.cwd()
     if domain:
         generate(base, domain)
 
